a59b1d4e4c9aa8a0916c2ebcb788f709e931850b
Kessy Abarenkov <kessy.abarenkov@gmail.com>
2021-05-21 16:32:29 +0300

    Update files

--- run_setup.sh
+++ run_setup.sh
@@ -15,15 +15,15 @@ chmod 755 protax.sif
 echo "Removing old db files ..."
 rm -fr db_files/
 
-echo "Downloading PROTAX-fungi supporting files from https://plutof.ut.ee/#/filerepository/view/4329518 ..."
+echo "Downloading PROTAX-fungi supporting files from https://plutof.ut.ee/#/filerepository/view/4333159 ..."
 wget https://files.plutof.ut.ee/public/orig/F5/B1/F5B1921D065CCEBD4E646300CC9D5B8D67045A9948E6E6910378EF00DD6F7B1F.zip
 unzip F5B1921D065CCEBD4E646300CC9D5B8D67045A9948E6E6910378EF00DD6F7B1F.zip
 rm F5B1921D065CCEBD4E646300CC9D5B8D67045A9948E6E6910378EF00DD6F7B1F.zip
 
 # Download db_files (1 & 2)
-echo "Downloading PROTAX-fungi supporting files (1) from https://plutof.ut.ee/#/filerepository/view/4329824 ..."
+echo "Downloading PROTAX-fungi supporting files (1) from https://plutof.ut.ee/#/filerepository/view/4333390 ..."
 wget https://files.plutof.ut.ee/public/orig/18/3C/183CA4119F32835273A28F765568DFCCC2302CAC246F14D1306DD2B9C4A753F8.zip
-echo "Downloading PROTAX-fungi supporting files (2) from https://plutof.ut.ee/#/filerepository/view/4329807 ..."
+echo "Downloading PROTAX-fungi supporting files (2) from https://plutof.ut.ee/#/filerepository/view/4333384 ..."
 wget https://files.plutof.ut.ee/public/orig/99/26/99265DB6F4D71CD7D8A96C974BB69B71B714A53DE218225073BB88D6810B5ECB.zip
 
 mv 183CA4119F32835273A28F765568DFCCC2302CAC246F14D1306DD2B9C4A753F8.zip db_files/
